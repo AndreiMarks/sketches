cd579456588a1afd3125e3361cb607a7465a1d63
Andrei Marks <andrei@weplaydots.com>
2017-02-25 01:35:01 -0500

    Relocate assets, space buttons.

---
+++ Assets/projects/reflections/scripts/MaximinerButtonPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MaximinerButtonPanel : ButtonPanel
+{
+    protected override Dictionary<string, Action> _ButtonDict 
+    { 
+        get 
+        {
+            if ( _buttonDict == null )
+            {
+                _buttonDict = new Dictionary<string, Action>() { 
+                                                        //{ "Timer", DoTimer },
+                                                        //{ "ChooseIt", DoChooseIt },
+                                                        //{ "Reflections", DoReflections },
+                                                        };
+            }
+
+            return _buttonDict;
+        } 
+    }
+}
